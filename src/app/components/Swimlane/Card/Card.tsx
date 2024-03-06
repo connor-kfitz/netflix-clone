1876488c358b360771217ab585f53917dc0dc75d
connor-kfitz <connorkfitzsimmons@gmail.com>
2024-03-05 23:28:16 -0500

    Added More Styling To Cards

--- src/app/components/Swimlane/Card/Card.tsx
+++ src/app/components/Swimlane/Card/Card.tsx
@@ -9,6 +9,8 @@ export default function Card({ cardData }: CardProps) {
   return (
     <a className="card">
       <img className="card__image" src={cardData.thumbImage}/>
+      <div className="card__gradient"></div>
+      <div className="card__title">{cardData.title}</div>
     </a>
   )
 }

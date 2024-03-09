64c5b2ae743eab84d38307c1fc56f7c0e99a2649
connor-kfitz <connorkfitzsimmons@gmail.com>
2024-03-08 23:16:17 -0500

    Added Sign Up Page w/ Functionality

--- src/app/components/SignUp/SignUp.tsx
+++ src/app/components/SignUp/SignUp.tsx
@@ -1,10 +1,10 @@
 "use client";
 import BackgroundImage from "../../../../public/images/login/login-background.jpg";
 import Image from "next/image";
-import "./Login.scss";
 import { useState } from "react";
 import { UserAuth } from "@/app/context/AuthContext";
 import { useRouter } from "next/navigation";
+import "./SignUp.scss";
 
 export default function Login() {
 
@@ -14,7 +14,7 @@ export default function Login() {
     password: ''
   })
 
-  const { logIn } = UserAuth();
+  const { signUp } = UserAuth();
 
   const router = useRouter();
 
@@ -26,40 +26,39 @@ export default function Login() {
   async function handleSubmit(event: React.MouseEvent<HTMLButtonElement>): Promise<void> {
     event.preventDefault();
     try {
-      await logIn(formData.email, formData.password);
+      await signUp(formData.email, formData.password);
       router.push('/browse');
     } catch (error) {
-      // Todo: Alert user that the login failed
+      // Todo: Alert user that the sign up failed
       console.log(error);
     }
   }
 
   return (
     // Todo: Fix some of the styling, on / off states for inputs
-    <div className="login">
-      <Image className="login__background-image" src={BackgroundImage} alt="Background"/>
-      <div className="login__background-gradient"></div>
-      <div className="login__container"> 
-        <form className="login-form">
-          <h1 className="login-form__title">Sign In</h1>
-          <label className="login-form__email-label">
-            <div className="login-form__email-title">Email</div>
+    <div className="sign-up">
+      <Image className="sign-up__background-image" src={BackgroundImage} alt="Background"/>
+      <div className="sign-up__background-gradient"></div>
+      <div className="sign-up__container"> 
+        <form className="sign-up-form">
+          <h1 className="sign-up-form__title">Sign Up</h1>
+          <label className="sign-up-form__email-label">
+            <div className="sign-up-form__email-title">Email</div>
             <input name="email" value={formData.email} onChange={handleFormChange} type="email"/>
           </label >
-          <label className="login-form__password-label">
-            <div className="login-form__password-title">Password</div>
+          <label className="sign-up-form__password-label">
+            <div className="sign-up-form__password-title">Password</div>
             <input value={formData.password} name="password" onChange={handleFormChange} type="password"/>
           </label>
-          <button className="login-form__submit-button" type="submit" onClick={handleSubmit}>Sign In</button>
-          <div className="login-form__help-container">
+          <button className="sign-up-form__submit-button" type="submit" onClick={handleSubmit}>Sign Up</button>
+          <div className="sign-up-form__help-container">
             {/* Todo: Make Custom Checkbox to Update Style */}
-            <label className="login-form__remember-me-label">
-              <input className="login-form__remember-me-checkbox" type="checkbox"/>
+            <label className="sign-up-form__remember-me-label">
+              <input className="sign-up-form__remember-me-checkbox" type="checkbox"/>
               Remember me
             </label>
-            <a className="login-form__help-text">Need help?</a>
+            <a className="sign-up-form__help-text">Need help?</a>
           </div>
-          <div className="login__new-user-text">New to Netflix?<a className="login__sign-up-link" href="/sign-up">Sign up now</a></div>
         </form>
       </div>
     </div>
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from "../components/SignUp/SignUp";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <SignUp/>
+    </>
+  )
+}
